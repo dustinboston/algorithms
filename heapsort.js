504f36927354a8943f501e399da7ce215994e760
anonymous <anonymous>
2019-03-30 20:14:58 -0700

    Add priority queue to heap

--- heapsort.js
+++ heapsort.js
@@ -1,5 +1,5 @@
 function parent(i) {
-  return Math.floor(i/2);
+  return Math.floor(i / 2);
 }
 
 function left(i) {
@@ -7,9 +7,48 @@ function left(i) {
 }
 
 function right(i) {
-  return (2 * i) + 1;
+  return 2 * i + 1;
 }
 
+// O(1)
+function heapMaximum(A) {
+  return A[0];
+}
+
+// O(log n)
+function heapExtractMax(A) {
+  if (A.heapSize < 0) {
+    throw new Error("heap underflow");
+  }
+  const max = A[0];
+  A[0] = A[A.heapSize];
+  A.heapSize = A.heapSize - 1;
+  maxHeapify(A, 0);
+  return max;
+}
+
+// O(log n)
+function heapIncreaseKey(A, i, key) {
+  if (key < A[i]) {
+    throw new Error("new key is smaller than current key");
+  }
+  A[i] = key;
+  while (i > 0 && A[parent(i)] < A[i]) {
+    let tmp = A[i];
+    A[i] = A[parent(i)];
+    A[parent(i)] = tmp;
+    i = parent(i);
+  }
+}
+
+// O(log n)
+function maxHeapInsert(A, key) {
+  A.heapSize = A.heapSize + 1;
+  A[A.heapSize] = -Infinity;
+  heapIncreaseKey(A, A.heapSize, key);
+}
+
+// O(log n)
 function maxHeapify(A, i) {
   let l = left(i);
   let r = right(i);
@@ -31,14 +70,16 @@ function maxHeapify(A, i) {
   }
 }
 
+// O(n), O(n log n)
 function buildMaxHeap(A) {
   A.heapSize = A.length - 1;
-  let i = Math.floor((A.length)/2);
+  let i = Math.floor(A.length / 2);
   for (i; i >= 0; i--) {
     maxHeapify(A, i);
   }
 }
 
+// O(n log n)
 function heapsort(A) {
   buildMaxHeap(A);
   let i;
@@ -51,4 +92,11 @@ function heapsort(A) {
   }
 }
 
-module.exports = heapsort;
+module.exports = {
+  buildMaxHeap,
+  heapsort,
+  heapExtractMax,
+  heapIncreaseKey,
+  heapMaximum,
+  maxHeapInsert
+};
--- heapsort.spec.js
+++ heapsort.spec.js
@@ -1,6 +1,34 @@
 const assert = require("assert");
-const heapsort = require("./heapsort");
+const {
+  buildMaxHeap,
+  heapExtractMax,
+  heapIncreaseKey,
+  heapMaximum,
+  heapsort,
+  maxHeapInsert
+} = require("./heapsort");
 
 let A = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7];
 heapsort(A);
-assert(A.toString() === '1,2,3,4,7,8,9,10,14,16');
+assert(A.toString() === "1,2,3,4,7,8,9,10,14,16");
+
+let A2 = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7];
+buildMaxHeap(A2);
+assert(A2.toString() === "16,14,9,10,8,1,4,2,3,7");
+assert(heapMaximum(A2) === 16);
+
+let A3 = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7];
+buildMaxHeap(A3);
+assert(heapExtractMax(A3) === 16);
+
+let A4 = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7];
+buildMaxHeap(A4);
+assert(A4.toString() === "16,14,9,10,8,1,4,2,3,7");
+heapIncreaseKey(A4, 6, 15); // A[6] => 4
+assert(A4.toString() === "16,15,9,14,8,1,10,2,3,7");
+
+let A5 = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7];
+buildMaxHeap(A5);
+assert(A5.toString() === "16,14,9,10,8,1,4,2,3,7");
+maxHeapInsert(A5, 15);
+assert(A5.toString() === "16,15,14,10,8,9,4,2,3,7,1");
