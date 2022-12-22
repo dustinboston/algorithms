09d140590255bc148376eda5f9e59064c05757b1
Dustin Boston <dboston@netflix.com>
2022-12-21 16:57:54 -0800

    Add longestPrefixOf, keys, keysWithPrefix

--- ternary-search-trie.js
+++ ternary-search-trie.js
@@ -59,10 +59,11 @@ class TernarySearchTrie {
   #getNode(x, key, d) {
     if (x === void 0) return;
     if (!key) throw new Error('A key is required');
-    const c = key.charAt(d);
-    if (c < x.c) { 
+    const c = key.charCodeAt(d);
+    const xc = x.c.charCodeAt(0);
+    if (c < xc) { 
       return this.#getNode(x.left, key, d);
-    } else if (c > x.c) {
+    } else if (c > xc) {
       return this.#getNode(x.right, key, d);
     } else if (d < key.length - 1) {
       return this.#getNode(x.mid, key, d + 1);
@@ -77,7 +78,7 @@ class TernarySearchTrie {
    * @param {any} val 
    */
   put(key, val) {
-    if (!key) throw new Error('A key is required');
+    if (key === void 0) throw new Error('A key is required');
     if (!this.contains(key)) {
       this.#n += 1;
     } else if (val === void 0) {
@@ -94,23 +95,98 @@ class TernarySearchTrie {
    * @param {number} d 
    */
   #putNode(x, key, val, d) {
-    const c = key.charAt(d);
     if (x === void 0) {
       x = new TernarySearchTrieNode();
-      x.c = c;
-    }
-    if (c < x.c) { 
-      x.left = this.#putNode(x.left, key, val, d);
-    } else if (c > x.c) {
-      return this.#putNode(x.right, key, val, d);
-    } else if (d < key.length - 1) {
-      return this.#putNode(x.mid, key, val, d + 1);
-    } else {
-      return x;
+      x.c = key.charAt(d);
     }
 
+    const c = key.charCodeAt(d);
+    const xc = x.c.charCodeAt(0);
+    if (c < xc) { 
+      x.left = this.#putNode(x.left, key, val, d);
+    } else if (c > xc) {
+      x.right = this.#putNode(x.right, key, val, d);
+    } else if (d < key.length - 1) {
+      x.mid = this.#putNode(x.mid, key, val, d + 1);
+    } else {
+      x.val = val;
+    }
+    return x;
   }
 
+  /**
+   * 
+   * @param {string} query 
+   * @returns {string}
+   */
+  longestPrefixOf(query) {
+    if (query === void 0) throw new Error('A query is required');
+    if (query.length === 0) return;
+
+    let length = 0;
+    let x = this.#root;
+
+    let i = 0;
+    while (x !== void 0 && i < query.length) {
+      const c = query.charCodeAt(i);
+      const xc = x.c.charCodeAt(0);
+      if (c < xc) { 
+        x = x.left;
+      } else if (c > xc) {
+        x = x.right;
+      } else {
+        i++;
+        if (x.val !== void 0) length = i;
+        x = x.mid;
+      }
+    }
+    return query.substring(0, length);
+  }
+
+  /**
+   * 
+   * @returns {string[]}
+   */
+  keys() {
+    /** @type {string[]} */
+    const q = [];
+    this.#collect(this.#root, '', q);
+    return q;
+  }
+
+  /**
+   * 
+   * @param {string} pre 
+   * @returns {string[]}
+   */
+  keysWithPrefix(pre) {
+    if (!pre || typeof pre !== 'string') {
+      throw new Error('A prefix string is required.')
+    }
+    /** @type {string[]} */
+    const q = [];
+    let x = this.#getNode(this.#root, pre, 0);
+    if (x === void 0) return q;
+    if (x.val !== void 0) q.push(pre);
+    this.#collect(x.mid, pre, q);
+    return q;
+  }
+
+  /**
+   * 
+   * @param {TernarySearchTrieNode} x 
+   * @param {string} pre 
+   * @param {string[]} q
+   */
+  #collect(x, pre, q) {
+    if (x === void 0) return;
+    this.#collect(x.left, pre, q);
+    if (x.val !== void 0) q.push(pre + x.c);
+    pre += x.c;
+    this.#collect(x.mid, pre, q);
+    pre = pre.substring(0, pre.length - 1);
+    this.#collect(x.right, pre, q);
+  }
 }
 
 module.exports = TernarySearchTrie;
--- ternary-search-trie.spec.js
+++ ternary-search-trie.spec.js
@@ -3,13 +3,68 @@ const TernarySearchTrie = require('./ternary-search-trie.js');
 
 function itShouldInsert() {
   const t = new TernarySearchTrie();
+  const key = 'she sells sea shells by the sea shore';
+  const keys = key.split(' ');
+  const uniqueKeys = {};
+  keys.forEach((k, i) => {
+    uniqueKeys[k] = true;
+    t.put(k, i);
+  });
+  const actual = t.size();
+  const expected = Object.keys(uniqueKeys).length;
+  assert(actual === expected);
+}
+itShouldInsert();
 
+function itShouldGetStringsByLongestPrefix() {
+  const t = new TernarySearchTrie();
   const key = 'she sells sea shells by the sea shore';
   const keys = key.split(' ');
   keys.forEach((k, i) => {
     t.put(k, i);
   });
-
-  assert(t.size() === keys.length);
+  
+  [
+    ['she', 'she'],
+    ['shell', 'she'],
+    ['shellsort', 'shells'],
+    ['shelter', 'she']
+  ].forEach(([input, expected]) => {
+    const actual = t.longestPrefixOf(input);
+    assert(actual === expected);
+  });
 }
-itShouldInsert();
+itShouldGetStringsByLongestPrefix();
+
+function itShouldGetKeys() {
+  const t = new TernarySearchTrie();
+  const key = 'she sells sea shells by the sea shore';
+  const keys = key.split(' ');
+  const uniqueKeys = {};
+  keys.forEach((k, i) => {
+    uniqueKeys[k] = true;
+    t.put(k, i);
+  });
+  const actual = t.keys().join(',');
+  const expected = Object.keys(uniqueKeys).sort().join(',');
+  assert(actual === expected);
+}
+itShouldGetKeys();
+
+function itShouldGetKeysWithPrefix() {
+  const t = new TernarySearchTrie();
+  const wordList = 'she sells sea shells by the sea shore';
+  const prefix = 'sh';
+  const matches = {};
+  wordList.split(' ').forEach((k, i) => {
+    if (k.length >= prefix.length && k.startsWith(prefix)) {
+      matches[k] = true;
+    }
+    t.put(k, i);
+  });
+
+  const actual = t.keysWithPrefix(prefix).join(',');
+  const expected = Object.keys(matches).join(',');
+  assert(actual === expected);
+}
+itShouldGetKeysWithPrefix();
